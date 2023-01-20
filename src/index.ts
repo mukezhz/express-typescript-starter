6a56e67f821b906f3efc4688512df0e802258794
Mukesh Kr. Chaudhary <mukezhz@gmail.com>
2023-01-20 08:07:21 +0545

    fix: changes app.ts to index.ts for starting file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 const port: number = 8080
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+    return res.send('Hello World')
 })
 
 app.listen(port, function () {
